e84980e9471a5a5434b11c8f1fb906278ef70f59
Aluis Marte <aluismarte@gmail.com>
2019-12-10 19:20:27 -0400

    Starting proyect.

---
+++ README.md
@@ -0,0 +1,5 @@
+# MemeGeneratorVaadinChallenge
+
+Meme creator Open write on Vaadin 14 to create your amazing memes!
+
+GitHub Holiday Coding Challenge 2019 `#vaadin` `#GitHubPack`
---
+++ src/main/java/com/alsjava/challenge/memegenerator/MemeGeneratorVaadinChallenge.java
@@ -0,0 +1,15 @@
+package com.alsjava.challenge.memegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aluis on 12/10/19.
+ */
+@SpringBootApplication
+public class MemeGeneratorVaadinChallenge {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeGeneratorVaadinChallenge.class, args);
+    }
+}
